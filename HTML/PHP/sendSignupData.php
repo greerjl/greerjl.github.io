d87d6faffeba6c355499cec618ae29784099b6b3
greerjl <greerjl@plu.edu>
2018-03-13 15:04:22 -0700

    Deleted old code, added headshot, edited index

--- HTML/PHP/sendSignupData.php
+++
@@ -1,3 +0,0 @@
-<?php
-	'uname' => $uname,
-?>
--- HTML/dbconnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-      define('DB_SERVER', '152.117.180.234:3306');
-      define('DB_USERNAME', 'remote');
-      define('DB_PASSWORD', 'getserved69');
-      define('DB_DATABASE', 'sys');
-      $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
